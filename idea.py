2e6ad7c1607df72f9ecd220d11cf90c4e3dda71e
xuchen <chen.xu@geelink.cn>
2021-06-23 20:55:28 +0800

    IDEA

--- idea.py
+++ idea.py
@@ -71,7 +71,7 @@ def load_kw_model(load_kw_prediction_path, device, use_keywords=True):
             kw_model = globals()[kw_model](**kw_model_kwargs)
         kw_model.load_state_dict(kw_model_checkpoint)
         kw_model.eval()  # set to evaluation mode, no training required
-        return kw_model
+        return kw_model.to(device)
 
 
 ## kw model forward
