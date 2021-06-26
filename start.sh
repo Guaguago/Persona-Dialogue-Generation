b2d94f0e39f4a37e146cfd9bb008abb07a129809
xuchen <chen.xu@geelink.cn>
2021-06-26 19:19:04 +0800

    start

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/p2/console.log
-#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/p2/eval_f1.log
+#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/p2/console.log
+python -u eval_f1.py > /apdcephfs/share_916081/chencxu/p2/eval_f1.log
