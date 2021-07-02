44f4ebd44cf1976d823ed91bf2afd31e2d9fe908
xuchen <chen.xu@geelink.cn>
2021-07-02 21:29:01 +0800

    172

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-python train_transmitter.py
-#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/train-o-l643-wj172.log
+python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/train-o-l643-wj172.log
 #python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/f1-o-l643-wj172.log
 #python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/hits-o-l643-wj172.log
