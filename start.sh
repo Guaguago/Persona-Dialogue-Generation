302621f5675662ce6e4a0d5ffe3388bc95df5247
xuchen <chen.xu@geelink.cn>
2021-07-02 12:37:03 +0800

    IDEA

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/p2/logs/train-o-l643-wj172.log
-#python -u eval_f1.py > /apdcephfs/share_916081/chencxu/p2/logs/f1-o-l643-wj172.log
-#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/p2/logs/hits-o-l643-wj172.log
+python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/train-o-l643-wj172.log
+#python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/f1-o-l643-wj172.log
+#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/hits-o-l643-wj172.log
