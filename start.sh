96b3de79c62512eaed8e385c3765716a7b59b56f
xuchen <chen.xu@geelink.cn>
2021-08-21 11:00:58 +0800

    start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/train-o-l1155.log
+#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/train-o-l1.log
 #python -u train_psquare.py > /apdcephfs/share_916081/chencxu/pegg/logs/2pegg/train-o-11155.log
 #python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/f1-o-l1155.log
-#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/hits-o-11155.log
-python -u sample_visualization.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/vis-o-11155.log
+#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/hits-o-l1.log
+#python -u sample_visualization.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/vis-o-11155.log
