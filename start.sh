c327998f7012cef814b54f279070c9921e5655e2
xuchen <chen.xu@geelink.cn>
2021-08-25 19:30:55 +0800

    start

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 #python -u train_psquare.py > /apdcephfs/share_916081/chencxu/pegg/logs/2pegg/train-o-11155.log
 #python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/f1-o-l1155.log
 #python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/hits-o-l1.log
+python -u eval_c_recall.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/cr-o-l5.log
 #python -u sample_visualization.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/vis-o-11155.log
