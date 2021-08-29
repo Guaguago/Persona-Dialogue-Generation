02813149bbb8be2c8af6148e668de239efb90958
xuchen <chen.xu@geelink.cn>
2021-08-29 23:24:01 +0800

    1

--- train_transmitter.py
+++ train_transmitter.py
@@ -80,7 +80,7 @@ def setup_args():
     select_persona = False
     shuffle_persona = True
     share_decoder_input_output_embed = False
-    num_train_epochs = 8
+    num_train_epochs = 4
 
     if ARCH_CHOICE == 'gpt':
         batchsize, lr, optimizer, gradient_clip = gpt_setting()
