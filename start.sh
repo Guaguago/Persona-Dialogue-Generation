b328fd08c23b4e76ff2e76f4dfc93beb85fe460e
xuchen <chen.xu@geelink.cn>
2021-09-03 23:13:59 +0800

    1

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/train-o-l1.log
-#python -u train_psquare.py > /apdcephfs/share_916081/chencxu/pegg/logs/2pegg/train-o-11155.log
-#python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/f1-o-l1155.log
-#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/hits-o-l1.log
-python -u eval_c_recall.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/cr-o-l5.log
-#python -u sample_visualization.py > /apdcephfs/share_916081/chencxu/pegg/logs/pegg/vis-o-11155.log
+#python -u train_transmitter.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/train-o-42.log
+#python -u train_psquare.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/train-o-42.log
+#python -u eval_f1.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/f1-o-42.log
+#python -u eval_hits.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/hits-o-42.log
+python -u eval_c_recall.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/cr-o-42.log
+#python -u sample_visualization.py > /apdcephfs/share_916081/chencxu/pegg/logs/AAAI/vis-o-42.log
