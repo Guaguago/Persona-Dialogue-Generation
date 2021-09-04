8b55fd88c3e1e98628147469d95084d06a882690
xuchen <chen.xu@geelink.cn>
2021-09-04 20:32:14 +0800

    1

--- agents/psquare/psquare.py
+++ agents/psquare/psquare.py
@@ -1161,9 +1161,8 @@ class PSquareAgent(Agent):
         obs = [{'text': c} for c in batch_messages]
         xs, _, _, sort_ind, *_ = PaddingUtils.pad_text(obs, self.dict,
                                                        null_idx=self.dict.pad_idx,
-                                                       dq=True, eval_labels=True,
-                                                       encode_truncate=self.encode_max_seq_len,
-                                                       decode_truncate=self.decode_max_seq_len)
+                                                       dq=False, eval_labels=True,
+                                                       encode_truncate=20)
         xs = split_pad_vector(xs, self.dict.end_idx, self.dict.pad_idx)
         xs = torch.LongTensor(xs)
         cuda_device = next(self.language_model.transformer_module.parameters()).device
