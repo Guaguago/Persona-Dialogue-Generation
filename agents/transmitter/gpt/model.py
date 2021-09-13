59c2307b9712b78a889884a8c5897c9bee42d799
xuchen <chen.xu@geelink.cn>
2021-09-13 16:23:45 +0800

    1

--- agents/transmitter/gpt/model.py
+++ agents/transmitter/gpt/model.py
@@ -571,7 +571,7 @@ class Gpt2SeqModel(nn.Module):
             for step in range(self.longest_label):
                 outputs, hidden_states = self.transformer_module.forward(token_tensor)
                 logits = outputs[:, -1, :]
-                lm_word_probs = cal_lm_word_probs(logits=logits.unsqueeze(1), softmax=self.softmax, temperature=1.5)
+                lm_word_probs = cal_lm_word_probs(logits=logits.unsqueeze(1), softmax=self.softmax, temperature=2.0)
 
                 if use_attention:
                     concept_word_probs = cal_concept_word_probs_attention(
